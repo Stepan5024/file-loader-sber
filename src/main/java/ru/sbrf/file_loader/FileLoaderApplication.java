087162ed103ec7a2bf1b8fa379377df68341be79
Степан <bokarevstepan1@yandex.ru>
2024-09-25 18:16:39 +0300

    init

---
+++ src/main/java/ru/sbrf/file_loader/FileLoaderApplication.java
@@ -0,0 +1,13 @@
+package ru.sbrf.file_loader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileLoaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sbrf/file_loader/FileLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sbrf.file_loader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
